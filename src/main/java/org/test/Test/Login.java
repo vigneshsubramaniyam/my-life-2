5147b5924a1065cc20e766b808c3dfe16212d1b0
vigneshsubramaniyam <vigneshwaran6902@gmail.com>
2021-12-31 16:39:36 +0530

    user nane functionality

---
+++ src/main/java/org/test/Test/Login.java
@@ -0,0 +1,12 @@
+package org.test.Test;
+
+public class Login {
+
+public static void userName() {
+System.out.println("userName");
+}
+
+public static void main(String[] args) {
+	userName();
+}
+}
