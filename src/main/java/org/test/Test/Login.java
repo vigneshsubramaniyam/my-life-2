c7a848e27b81e639a7235de27cceea5ae231dc9e
vigneshsubramaniyam <vigneshwaran6902@gmail.com>
2021-12-31 16:50:12 +0530

    password functionaly

---
+++ src/main/java/org/test/Test/Login.java
@@ -0,0 +1,13 @@
+package org.test.Test;
+
+public class Login {
+
+public static void passWord() {
+System.out.println("password");
+}	
+
+public static void main(String[] args) {
+passWord();
+}
+}
+
